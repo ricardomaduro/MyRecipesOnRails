67d543da3a761b05f47c20ce375d307aff672455
ricardomaduro <ricardo.s.maduro@gmail.com>
2015-05-01 15:38:31 +0000

    some more improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    
+    def gravatar_for( chef, options = {size: 80})
+        #gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
+        #size = options[:size]
+        #gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        #gravatar_url =""
+        #image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
+    end
 end
