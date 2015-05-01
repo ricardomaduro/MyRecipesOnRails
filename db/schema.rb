c2ab515a64246c494222f5aa16baa564ece52260
ricardomaduro <ricardo.s.maduro@gmail.com>
2015-05-01 16:27:03 +0000

    images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428153504) do
+ActiveRecord::Schema.define(version: 20150501154640) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150428153504) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
 end
