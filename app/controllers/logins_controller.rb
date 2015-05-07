8f2b661e181c6cc2bbb2da79c3c735ef6d9901b7
ricardomaduro <ricardo.s.maduro@gmail.com>
2015-05-07 21:10:50 +0000

    login and look

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,25 @@
+class LoginsController < ApplicationController
+    
+    def new
+        
+    end
+    
+    def create
+        
+        chef = Chef.find_by(email: params[:email])
+        if chef && chef.authenticate(params[:password])
+            session[:chef_id] = chef.id
+            flash[:success] = "Your are logged in"
+            redirect_to recipes_path
+        else
+            flash.now[:danger] = "Your password or email do not match"
+            render new
+        end
+    end
+    
+    def destroy
+        session[:chef_id] = nil
+        flash[:success] = "You have logged out"
+    end
+    
+end
