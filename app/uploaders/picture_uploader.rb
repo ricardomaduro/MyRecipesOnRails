9fdda25cfceda568422ec7fe6bb5dce1b4f937eb
ricardomaduro <ricardo.s.maduro@gmail.com>
2015-05-09 20:02:20 +0000

    aws changes but commented

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,6 +10,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  #if Rails.env.production?
+  #  storage :fog
+  #else
+  #  storage :file
+  #end
+  
   storage :file
   # storage :fog
 
